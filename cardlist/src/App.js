8ee88c203a23f81847ef85f7d69976ebc721e265
Stian Reistad Røgeberg <srr@dips.no>
2023-12-11 09:04:05 +0100

    added CardList and Form component

--- cardlist/src/App.js
+++ cardlist/src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
 
+const CardList = (props) => (
+  <div>
+    {}
+  </div>
+);
+
 class Card extends React.Component {
 
+}
+
+class From extends React.Component {
+  
 }
 
 class App extends React.Component {
