108e677c72e911cead0099ef1e168b4867b9b635
Stian Reistad Røgeberg <srr@dips.no>
2023-12-11 09:09:44 +0100

    adding code to App commponent with properties

--- cardlist/src/App.js
+++ cardlist/src/App.js
@@ -10,12 +10,29 @@ class Card extends React.Component {
 
 }
 
-class From extends React.Component {
+class Form extends React.Component {
 
 }
 
 class App extends React.Component {
-  
+  state = {
+    profiles: []
+  };
+
+  addNewProfile = (profileData) => {
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData]
+    }));    
+  };
+  render () {
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
+        <Form onSubmit={this.addNewProfile} />
+        <CardList profiles={this.state.profiles} />
+      </div>
+    );
+  }
 }
 
 export default App;
