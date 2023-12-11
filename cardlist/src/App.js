c455e1cf301616a9c3919b7a2d0220656df7b24d
Stian Reistad Røgeberg <srr@dips.no>
2023-12-11 09:25:54 +0100

    First version of working app

--- cardlist/src/App.js
+++ cardlist/src/App.js
@@ -1,17 +1,50 @@
 import './App.css';
+import React, { useRef, useState } from 'react';
+import axios from "axios";
 
 const CardList = (props) => (
   <div>
-    {}
+    {props.profiles.map(profile => <Card key={profile.id} {...profile}/>)}
   </div>
 );
 
 class Card extends React.Component {
-
+  render() {
+    const profile = this.props;
+    return (
+      <div className='github-profile'>
+        <img src={profile.avatar_url} />
+        <div className='info'>
+          <div className='name'>{profile.name}</div>
+          <div className='company'>{profile.company}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 class Form extends React.Component {
+  state = { userNane: "" };
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const resp = await axios.get(`https://github.com/users/${this.state.userNane}`);
+    this.props.onSubmit(resp.data);
+    this.setState({ userNane: "" });
+  };
 
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.userNane}
+          onChange={event => this.setState({ userNane: event.target.value })}
+          placeholder='Search username'
+          required />
+          <button>Add Card</button>
+      </form>
+    );
+  }
 }
 
 class App extends React.Component {
